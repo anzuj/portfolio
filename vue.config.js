67b963701a44e212393b079c7326d1666c2a534f
Anzelika Häberli <anzelika@joineer.ch>
2021-01-25 16:11:41 +0100

    public path fix

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     transpileDependencies:  ['vuetify', 'dotenv'],
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfolio/'
-    : '/',
     pluginOptions: {
       i18n: {
         locale: 'en',
